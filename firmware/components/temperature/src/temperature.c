ab0ab030b7e32419c88dc238622554393b4a2a71
PabloDev2195 <chavezpablocesar58@gmail.com>
2026-09-21 21:07:57 -0600

    Chane task execution from main to temperature module

--- firmware/components/temperature/src/temperature.c
+++ firmware/components/temperature/src/temperature.c
@@ -1,7 +1,65 @@
 #include "temperature.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+
 #include "adc.h"
+#include "ble_mgr.h"
+
+#define TEMPERATURE_TASK_STACK_SIZE    2048
+#define TEMPERATURE_TASK_PRIORITY      2
+#define TEMPERATURE_QUEUE_LENGTH       1
 
 #define TEMPERATURE_SENSOR_SCALING_FACTOR 100.0f
+
+temperature_status_t temperature_read(float *temperature);
+
+
+/**
+ * @brief FreeRTOS task for temperature monitoring.
+ *
+ * Periodically reads the temperature sensor and updates
+ * the current temperature value.
+ *
+ * @param pvParameters Task parameters (unused).
+ */
+static void temperature_task(void *pvParameters)
+{
+    float temp_c;
+    for (;;) 
+    {
+        temperature_status_t status = temperature_read(&temp_c);
+
+        if (status == TEMPERATURE_STATUS_OK) 
+        {
+            ble_manager_update_temperature(temp_c);
+        } 
+        else 
+        {
+
+        }
+        vTaskDelay(pdMS_TO_TICKS(200));
+    }
+}
+
+/**
+ * @brief Creates the temperature task.
+ *
+ * The task reads the temperature sensor and updates the BLE manager.
+ */
+void temperature_task_create(void)
+{
+    xTaskCreate(
+        temperature_task,
+        "temperature_task",
+        TEMPERATURE_TASK_STACK_SIZE,
+        NULL,
+        TEMPERATURE_TASK_PRIORITY,
+        NULL
+    );
+}
+
 /**
  * @brief Read the current temperature.
  *
